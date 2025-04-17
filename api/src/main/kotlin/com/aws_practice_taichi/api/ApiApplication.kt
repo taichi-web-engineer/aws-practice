cc0f6bd7330ed5a4b04307127cabf713b84afc4a
taichi-web-engineer <aogaku415@gmail.com>
2025-04-17 20:58:17 +0900

    バックエンドAPIのプロジェクト作成

---
+++ api/src/main/kotlin/com/aws_practice_taichi/api/ApiApplication.kt
@@ -0,0 +1,11 @@
+package com.aws_practice_taichi.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiApplication>(*args)
+}
---
+++ api/src/test/kotlin/com/aws_practice_taichi/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aws_practice_taichi.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
