9824a750e1d08516d84459d495eaecc733cb1e6d
taichi-web-engineer <aogaku415@gmail.com>
2025-04-28 20:52:28 +0900

    controllerのアノテーションを適切に修正

--- api/src/main/kotlin/com/awsPracticeTaichi/api/presentation/ApiController.kt
+++ api/src/main/kotlin/com/awsPracticeTaichi/api/presentation/ApiController.kt
@@ -1,14 +1,12 @@
 package com.awsPracticeTaichi.api.presentation
 
 import com.awsPracticeTaichi.api.usecase.ApiUsecase
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@EnableAutoConfiguration
 class ApiController(private val apiUsecase: ApiUsecase) {
-    @RequestMapping("/")
+    @GetMapping("/")
     fun home(): List<String> {
         return apiUsecase.getData()
     }
